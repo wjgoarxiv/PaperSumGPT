10796281e69dfd07db5f0c30c411cb2a54203335
wjgoarxiv <118244317+wjgoarxiv@users.noreply.github.com>
2023-06-21 14:17:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="PaperSumGPT",
-    version="1.1.0",
+    version="1.2.0",
     author="wjgoarxiv",
     author_email="woo_go@yahoo.com",
     description="A tool to abbreviate scientific paper contents using ChatGPT",

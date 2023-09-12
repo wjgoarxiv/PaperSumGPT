2d5ad88f9ff750fe52856aefe7ad30cf55dc1763
wjgoarxiv <118244317+wjgoarxiv@users.noreply.github.com>
2023-09-12 15:41:27 +0900

    Update install_old-repo.sh
    The original author changed their directory name, which made a significant error while installing the old repo using the `./install_old-repo.sh` file.

--- install_old-repo.sh
+++ install_old-repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# IMPORTANT: We have to download the old-repo of chatgpt-wrapper; since this repo is the last version that supports the browser-based chats.
+# IMPORTANT: We have to download the old repo of `chatgpt-wrapper`; since this repo is the last version that supports browser-based chats.
 
 # Authorize
 chmod +x *
@@ -8,7 +8,7 @@ chmod +x *
 # Download the old-repo
 wget https://github.com/mmabrouk/chatgpt-wrapper/archive/5de50d92d2b9937165f22463350b1fac660e8e54.zip;
 unzip 5de50d92d2b9937165f22463350b1fac660e8e54.zip;
-cd chatgpt-wrapper-5de50d92d2b9937165f22463350b1fac660e8e54;
+cd llm-workflow-engine-5de50d92d2b9937165f22463350b1fac660e8e54/;
 
 # Install the old-repo
 pip install -e . --quiet;

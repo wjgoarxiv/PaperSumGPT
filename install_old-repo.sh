05c8863f967d82123ca0978234d7bda04f1df0c9
wjgoarxiv <118244317+wjgoarxiv@users.noreply.github.com>
2023-11-20 13:39:00 +0900

    Bug fixed; since `chatgpt-wrapper` repo has been removed from the original Github link.

--- install_old-repo.sh
+++ install_old-repo.sh
@@ -5,10 +5,9 @@
 # Authorize
 chmod +x *
 
-# Download the old-repo
-wget https://github.com/mmabrouk/chatgpt-wrapper/archive/5de50d92d2b9937165f22463350b1fac660e8e54.zip;
-unzip 5de50d92d2b9937165f22463350b1fac660e8e54.zip;
-cd llm-workflow-engine-5de50d92d2b9937165f22463350b1fac660e8e54/;
+# Unzip the old-repo! 
+unzip main.zip;
+cd main/;
 
 # Install the old-repo
 pip install -e . --quiet;
